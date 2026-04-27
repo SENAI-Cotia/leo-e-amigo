77cb6083861bb55540a970a3ded8bb14ad9bcfd2
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-04-27 14:48:13 -0300

    Estrutura Spring

---
+++ src/main/java/br/com/cinestar/CinestarApplication.java
@@ -0,0 +1,13 @@
+package br.com.cinestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinestarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cinestar/CinestarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cinestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
