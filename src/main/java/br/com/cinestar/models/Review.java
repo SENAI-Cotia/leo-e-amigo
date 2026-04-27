76a658deaabb1528c5263f47899fe9ec7814cabc
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-04-27 16:19:08 -0300

    Models

--- src/main/java/br/com/cinestar/models/Review.java
+++ src/main/java/br/com/cinestar/models/Review.java
@@ -1,4 +1,66 @@
 package br.com.cinestar.models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nomeFilme;
+    @Column(nullable = true, unique = false)
+    private String reviewDesc;
+    @Column(nullable = false, unique = false)
+    private Double nota;
+
+    @ManyToOne
+    private User user;
+
+    public Review() {
+    }
+
+    public Review(Long id, String nomeFilme, String reviewDesc, Double nota) {
+        this.id = id;
+        this.nomeFilme = nomeFilme;
+        this.reviewDesc = reviewDesc;
+        this.nota = nota;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeFilme() {
+        return nomeFilme;
+    }
+
+    public void setNomeFilme(String nomeFilme) {
+        this.nomeFilme = nomeFilme;
+    }
+
+    public String getReviewDesc() {
+        return reviewDesc;
+    }
+
+    public void setReviewDesc(String reviewDesc) {
+        this.reviewDesc = reviewDesc;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
 }
+
+
--- src/main/java/br/com/cinestar/models/User.java
+++ src/main/java/br/com/cinestar/models/User.java
@@ -1,4 +1,77 @@
 package br.com.cinestar.models;
 
-public class User {
-}
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+    @Table(name = "user")
+    public class User {
+        @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        private Long id;
+        @Column(nullable = false,length=100)
+        private String nome;
+        @Column(nullable = false, unique = true)
+        private String username;
+        @Column(nullable = false, unique = true)
+        private String email;
+        @Column(nullable = false,length=100)
+        private String senha;
+
+        @OneToMany(mappedBy = "user")
+        private List<Review> review;
+
+        public User() {
+        }
+
+        public User(Long id, String nome, String username, String email, String senha) {
+            this.id = id;
+            this.nome = nome;
+            this.username = username;
+            this.email = email;
+            this.senha = senha;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
+
+        public void setSenha(String senha) {
+            this.senha = senha;
+        }
+    }
+
+
