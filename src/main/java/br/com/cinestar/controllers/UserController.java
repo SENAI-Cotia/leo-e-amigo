d1dfd37c44d80e6c10962e4d6346feed0f90295f
Leonardo <leonardoherrerasabbatini@gmail.com>
2026-04-28 11:56:05 -0300

    AJUSTES TROCA DE PAGINA (INCOMPLETO - ERRO)

--- src/main/java/br/com/cinestar/controllers/UserController.java
+++ src/main/java/br/com/cinestar/controllers/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     @GetMapping
     public String getUsers(Model model){
         return "Login";
-    }
+}
 
     @GetMapping("/cadastro")
     public String formUser(Model model){
@@ -39,8 +39,8 @@ public class UserController {
     return "Index";
     }
 
-    @PostMapping("/logar")
-    public String salvar(@ModelAttribute User userR){
+    @PostMapping("/login")
+    public String logar(@ModelAttribute User userR){
         User user=userRepository.findByEmail(userR.getEmail()).orElseThrow(()-> new RuntimeException("Credenciais inválidas"));
         if (!user.getSenha().equals(userR.getSenha())){
             throw new RuntimeException("Credenciais inválidas");
@@ -49,7 +49,7 @@ public class UserController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(@ModelAttribute User userR, RedirectAttributes redirectAttributes) {
+    public String cadastrar(@ModelAttribute User userR, RedirectAttributes redirectAttributes) {
         if (userR.getEmail().trim().isEmpty() || userR.getNome().trim().isEmpty()) {
             redirectAttributes.addFlashAttribute("mensagemErro", "ERRO: Digite um email e nome válidos");
             return "redirect:/cadastro";
